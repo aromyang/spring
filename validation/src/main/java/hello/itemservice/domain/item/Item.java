fea4f07681272af16c013427a3e36cccf13d9c83
romcanrom <baros2san@gmail.com>
2022-07-22 00:39:46 +0900

    validation study (2) bean validation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,32 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격 * 수량의 합은 10000이 넘어야 합니다") //권장 X 직접 코드로 작성하자
 public class Item {
 
+    //Form 전송 객체를 분리하고 검증을 Form 객체에서 하기로 함
+
+
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
+
+//    @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
     private Integer price;
+
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
---
+++ validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,30 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        Item item = new Item();
+        item.setItemName(" "); //violation.getMessage() = 공백일 수 없습니다
+        item.setPrice(0); //violation.getMessage() = 1000에서 1000000 사이여야 합니다
+        item.setQuantity(10000); //violation.getMessage() = 9999 이하여야 합니다
+
+        Set<ConstraintViolation<Item>> violations = validator.validate(item);
+        for (ConstraintViolation<Item> violation : violations) {
+            System.out.println("violation = " + violation);
+            System.out.println("violation.getMessage() = " + violation.getMessage());
+        }
+    }
+}
